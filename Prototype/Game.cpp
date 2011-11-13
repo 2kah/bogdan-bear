0022c96107774ab3aa55bdd1ad66092aba73e77a
Ragzouken <Ragzouken@riddler.castle.grayskull.co.uk>
2011-11-13 22:07:51 +0000

    Beginning point for the prototype. Starting with the Game class. Main loop to go here and then everything else will plug in... eventually.

---
+++ Prototype/Game.cpp
@@ -0,0 +1,23 @@
+#include "Game.h"
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+#define WIN32_LEAN_AND_MEAN
+#include "windows.h"
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+    INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT )
+#else
+    int main(int argc, char *argv[])
+#endif
+    {
+        return 0;
+    }
+
+#ifdef __cplusplus
+}
+#endif
---
+++ Prototype/Game.h
@@ -0,0 +1,4 @@
+#ifndef __Game_h_
+#define __Game_h_
+
+#endif // #ifndef __Game_h_
