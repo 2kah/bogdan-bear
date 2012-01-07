989003dc22c87bdeab5fdbd84355f91969f7958b
Ragzouken <Ragzouken@riddler.castle.grayskull.co.uk>
2012-01-07 01:30:42 +0000

    Integrated working explosion physics, still requires refactor.

--- Prototype/ExplosionPhysics.cpp
+++ Prototype/ExplosionPhysics.cpp
@@ -1,6 +1,8 @@
 #include "ExplosionPhysics.h"
 
 #include <iostream>
+#include <vector>
+#include <set>
 
 #include <boost/math/constants/constants.hpp>
 
@@ -24,14 +26,19 @@ ExplosionPhysics::ExplosionPhysics(Explosion *explosion, btDiscreteDynamicsWorld
     this->ghost = new btGhostObject();
     
     btVector3 position(this->explosion->position.x, this->explosion->position.y, this->explosion->position.z);
+    this->ghost->setCollisionShape(new btSphereShape(100 / 16.0));
 
-    this->ghost->setCollisionShape(new btSphereShape(100));
     this->ghost->setCollisionFlags(btCollisionObject::CF_NO_CONTACT_RESPONSE);
     this->ghost->setWorldTransform(btTransform(btMatrix3x3::getIdentity(), position));
 
     this->dynamicsWorld->addCollisionObject(this->ghost, btBroadphaseProxy::SensorTrigger, btBroadphaseProxy::AllFilter & ~btBroadphaseProxy::SensorTrigger);
 
     std::cout << "Explosion destroyed " << this->ghost->getNumOverlappingObjects() << " objects!" << std::endl;
+
+    std::vector<btCollisionObject *> destructions;
+    std::set<unsigned> levels;
+
+    Tower *tower;
 
     for(int i = 0; i < this->ghost->getNumOverlappingObjects(); i++)
     {
@@ -40,11 +47,25 @@ ExplosionPhysics::ExplosionPhysics(Explosion *explosion, btDiscreteDynamicsWorld
         BlockReference *block = (BlockReference *) blockObject->getUserPointer();
 
         if (block != NULL) {
-            block->tower->blocks[block->layer][block->level][block->sector] = false;
-            block->tower->signals.levelUpdated(block->tower, block->level);
+            block->tower->blocks[block->level][block->layer][block->sector] = false;
+            levels.insert((block->level / 4) * 4);
+
+            tower = block->tower;
+
+            destructions.push_back(blockObject);
         }
+    }
+
+    for(std::vector<btCollisionObject *>::iterator it = destructions.begin(); it != destructions.end(); ++it) {
+        btCollisionObject *blockObject = *it;
 
         this->dynamicsWorld->removeCollisionObject(blockObject);
+    }
+
+    for(std::set<unsigned>::iterator i = levels.begin(); i != levels.end(); ++i) {
+        unsigned level = *i;
+        
+        tower->signals.levelUpdated(tower, level);
     }
 }
 
