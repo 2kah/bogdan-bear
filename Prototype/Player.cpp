96181500ed55ce32cb9b164f93df043f32a09658
Ragzouken <Ragzouken@riddler.lan.cogsbristol.co.uk>
2012-01-21 12:49:53 +0000

    Minor fixes to collisions for rockets

--- Prototype/Player.cpp
+++ Prototype/Player.cpp
@@ -83,7 +83,7 @@ void Player::fire(bool state)
         Ogre::Quaternion orientation = this->orientation * this->relativeAim;
         orientation = orientation * Ogre::Quaternion(Ogre::Degree(90), Ogre::Vector3::UNIT_Y);
 
-        this->signals.fired(this, new Rocket(this->position + Ogre::Vector3::UNIT_Y * 180 / 16.0, orientation));
+        this->signals.fired(this, new Rocket(this->position + Ogre::Vector3::UNIT_Y * 180 / 16.0 + (orientation * Ogre::Vector3::UNIT_X * 10), orientation));
     }
 }
 
