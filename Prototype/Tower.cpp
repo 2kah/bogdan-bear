cf8ce8645e2277b5a5793b571784ffb48ff070c5
Ragzouken <Ragzouken@riddler.lan.cogsbristol.co.uk>
2012-01-21 11:32:34 +0000

    Fixed graphics rebuilding being sloooow

--- Prototype/Tower.cpp
+++ Prototype/Tower.cpp
@@ -176,30 +176,30 @@ std::vector<BlockTriangle> ComplexTower::getBlockTriangles(unsigned level, unsig
     inner_anti_normal.normalise();
 
     BlockTriangle back1;
-    back1.points.push_back(points.b2);
-    back1.points.push_back(points.b1);
-    back1.points.push_back(points.d1);
+    back1.points[0] = points.b2;
+    back1.points[1] = points.b1;
+    back1.points[2] = points.d1;
 
-    back1.colours.push_back(Ogre::ColourValue::Blue);
-    back1.colours.push_back(Ogre::ColourValue::Green);
-    back1.colours.push_back(Ogre::ColourValue::White);
+    back1.colours[0] = Ogre::ColourValue::Blue;
+    back1.colours[1] = Ogre::ColourValue::Green;
+    back1.colours[2] = Ogre::ColourValue::White;
 
-    back1.normals.push_back(inner_clock_normal);
-    back1.normals.push_back(inner_clock_normal);
-    back1.normals.push_back(inner_anti_normal);
+    back1.normals[0] = inner_clock_normal;
+    back1.normals[1] = inner_clock_normal;
+    back1.normals[2] = inner_anti_normal;
 
     BlockTriangle back2;
-    back2.points.push_back(points.d1);
-    back2.points.push_back(points.d2);
-    back2.points.push_back(points.b2);
+    back2.points[0] = points.d1;
+    back2.points[1] = points.d2;
+    back2.points[2] = points.b2;
 
-    back2.colours.push_back(Ogre::ColourValue::White);
-    back2.colours.push_back(Ogre::ColourValue::Red);
-    back2.colours.push_back(Ogre::ColourValue::Blue);
+    back2.colours[0] = Ogre::ColourValue::White;
+    back2.colours[1] = Ogre::ColourValue::Red;
+    back2.colours[2] = Ogre::ColourValue::Blue;
 
-    back2.normals.push_back(inner_anti_normal);
-    back2.normals.push_back(inner_anti_normal);
-    back2.normals.push_back(inner_clock_normal);
+    back2.normals[0] = inner_anti_normal;
+    back2.normals[1] = inner_anti_normal;
+    back2.normals[2] = inner_clock_normal;
 
     triangles.push_back(back1);
     triangles.push_back(back2);
@@ -210,30 +210,30 @@ std::vector<BlockTriangle> ComplexTower::getBlockTriangles(unsigned level, unsig
     clock_normal.normalise();
 
     BlockTriangle clock_1;
-    clock_1.points.push_back(points.c2);
-    clock_1.points.push_back(points.c1);
-    clock_1.points.push_back(points.b1);
+    clock_1.points[0] = points.c2;
+    clock_1.points[1] = points.c1;
+    clock_1.points[2] = points.b1;
 
-    clock_1.colours.push_back(Ogre::ColourValue::Red);
-    clock_1.colours.push_back(Ogre::ColourValue::Blue);
-    clock_1.colours.push_back(Ogre::ColourValue::Green);
+    clock_1.colours[0] = Ogre::ColourValue::Red;
+    clock_1.colours[1] = Ogre::ColourValue::Blue;
+    clock_1.colours[2] = Ogre::ColourValue::Green;
 
-    clock_1.normals.push_back(clock_normal);
-    clock_1.normals.push_back(clock_normal);
-    clock_1.normals.push_back(clock_normal);
+    clock_1.normals[0] = clock_normal;
+    clock_1.normals[1] = clock_normal;
+    clock_1.normals[2] = clock_normal;
 
     BlockTriangle clock_2;
-    clock_2.points.push_back(points.b1);
-    clock_2.points.push_back(points.b2);
-    clock_2.points.push_back(points.c2);
+    clock_2.points[0] = points.b1;
+    clock_2.points[1] = points.b2;
+    clock_2.points[2] = points.c2;
 
-    clock_2.colours.push_back(Ogre::ColourValue::Red);
-    clock_2.colours.push_back(Ogre::ColourValue::Blue);
-    clock_2.colours.push_back(Ogre::ColourValue::Green);
+    clock_2.colours[0] = Ogre::ColourValue::Red;
+    clock_2.colours[1] = Ogre::ColourValue::Blue;
+    clock_2.colours[2] = Ogre::ColourValue::Green;
 
-    clock_2.normals.push_back(clock_normal);
-    clock_2.normals.push_back(clock_normal);
-    clock_2.normals.push_back(clock_normal);
+    clock_2.normals[0] = clock_normal;
+    clock_2.normals[1] = clock_normal;
+    clock_2.normals[2] = clock_normal;
 
     if (!this->blocks[level][layer][(sector - 1) % this->blocks[level][layer].size()]) {
         triangles.push_back(clock_1);
@@ -246,30 +246,30 @@ std::vector<BlockTriangle> ComplexTower::getBlockTriangles(unsigned level, unsig
     anti_normal.normalise();
 
     BlockTriangle anti_1;
-    anti_1.points.push_back(points.d2);
-    anti_1.points.push_back(points.d1);
-    anti_1.points.push_back(points.a1);
+    anti_1.points[0] = points.d2;
+    anti_1.points[1] = points.d1;
+    anti_1.points[2] = points.a1;
 
-    anti_1.colours.push_back(Ogre::ColourValue::Red);
-    anti_1.colours.push_back(Ogre::ColourValue::White);
-    anti_1.colours.push_back(Ogre::ColourValue::Red);
+    anti_1.colours[0] = Ogre::ColourValue::Red;
+    anti_1.colours[1] = Ogre::ColourValue::White;
+    anti_1.colours[2] = Ogre::ColourValue::Red;
 
-    anti_1.normals.push_back(anti_normal);
-    anti_1.normals.push_back(anti_normal);
-    anti_1.normals.push_back(anti_normal);
+    anti_1.normals[0] = anti_normal;
+    anti_1.normals[1] = anti_normal;
+    anti_1.normals[2] = anti_normal;
 
     BlockTriangle anti_2;
-    anti_2.points.push_back(points.a1);
-    anti_2.points.push_back(points.a2);
-    anti_2.points.push_back(points.d2);
+    anti_2.points[0] = points.a1;
+    anti_2.points[1] = points.a2;
+    anti_2.points[2] = points.d2;
 
-    anti_2.colours.push_back(Ogre::ColourValue::Red);
-    anti_2.colours.push_back(Ogre::ColourValue::White);
-    anti_2.colours.push_back(Ogre::ColourValue::Red);
+    anti_2.colours[0] = Ogre::ColourValue::Red;
+    anti_2.colours[1] = Ogre::ColourValue::White;
+    anti_2.colours[2] = Ogre::ColourValue::Red;
 
-    anti_2.normals.push_back(anti_normal);
-    anti_2.normals.push_back(anti_normal);
-    anti_2.normals.push_back(anti_normal);
+    anti_2.normals[0] = anti_normal;
+    anti_2.normals[1] = anti_normal;
+    anti_2.normals[2] = anti_normal;
 
     if (!this->blocks[level][layer][(sector + 1) % this->blocks[level][layer].size()]) {
         triangles.push_back(anti_1);
@@ -280,30 +280,30 @@ std::vector<BlockTriangle> ComplexTower::getBlockTriangles(unsigned level, unsig
     Ogre::Vector3 up_normal(Ogre::Vector3::UNIT_Y);
 
     BlockTriangle up_1;
-    up_1.points.push_back(points.c2);
-    up_1.points.push_back(points.b2);
-    up_1.points.push_back(points.d2);
+    up_1.points[0] = points.c2;
+    up_1.points[1] = points.b2;
+    up_1.points[2] = points.d2;
 
-    up_1.colours.push_back(Ogre::ColourValue::Green);
-    up_1.colours.push_back(Ogre::ColourValue::Blue);
-    up_1.colours.push_back(Ogre::ColourValue::Red);
+    up_1.colours[0] = Ogre::ColourValue::Green;
+    up_1.colours[1] = Ogre::ColourValue::Blue;
+    up_1.colours[2] = Ogre::ColourValue::Red;
 
-    up_1.normals.push_back(up_normal);
-    up_1.normals.push_back(up_normal);
-    up_1.normals.push_back(up_normal);
+    up_1.normals[0] = up_normal;
+    up_1.normals[1] = up_normal;
+    up_1.normals[2] = up_normal;
 
     BlockTriangle up_2;
-    up_2.points.push_back(points.d2);
-    up_2.points.push_back(points.a2);
-    up_2.points.push_back(points.c2);
+    up_2.points[0] = points.d2;
+    up_2.points[1] = points.a2;
+    up_2.points[2] = points.c2;
 
-    up_2.colours.push_back(Ogre::ColourValue::Red);
-    up_2.colours.push_back(Ogre::ColourValue::White);
-    up_2.colours.push_back(Ogre::ColourValue::Green);
+    up_2.colours[0] = Ogre::ColourValue::Red;
+    up_2.colours[1] = Ogre::ColourValue::White;
+    up_2.colours[2] = Ogre::ColourValue::Green;
 
-    up_2.normals.push_back(up_normal);
-    up_2.normals.push_back(up_normal);
-    up_2.normals.push_back(up_normal);
+    up_2.normals[0] = up_normal;
+    up_2.normals[1] = up_normal;
+    up_2.normals[2] = up_normal;
 
     triangles.push_back(up_1);
     triangles.push_back(up_2);
@@ -312,30 +312,30 @@ std::vector<BlockTriangle> ComplexTower::getBlockTriangles(unsigned level, unsig
     Ogre::Vector3 down_normal(-Ogre::Vector3::UNIT_Y);
 
     BlockTriangle down_1;
-    down_1.points.push_back(points.d1);
-    down_1.points.push_back(points.b1);
-    down_1.points.push_back(points.c1);
+    down_1.points[0] = points.d1;
+    down_1.points[1] = points.b1;
+    down_1.points[2] = points.c1;
 
-    down_1.colours.push_back(Ogre::ColourValue::White);
-    down_1.colours.push_back(Ogre::ColourValue::Green);
-    down_1.colours.push_back(Ogre::ColourValue::Blue);
+    down_1.colours[0] = Ogre::ColourValue::White;
+    down_1.colours[1] = Ogre::ColourValue::Green;
+    down_1.colours[2] = Ogre::ColourValue::Blue;
 
-    down_1.normals.push_back(down_normal);
-    down_1.normals.push_back(down_normal);
-    down_1.normals.push_back(down_normal);
+    down_1.normals[0] = down_normal;
+    down_1.normals[1] = down_normal;
+    down_1.normals[2] = down_normal;
 
     BlockTriangle down_2;
-    down_2.points.push_back(points.c1);
-    down_2.points.push_back(points.a1);
-    down_2.points.push_back(points.d1);
+    down_2.points[0] = points.c1;
+    down_2.points[1] = points.a1;
+    down_2.points[2] = points.d1;
 
-    down_2.colours.push_back(Ogre::ColourValue::Blue);
-    down_2.colours.push_back(Ogre::ColourValue::Red);
-    down_2.colours.push_back(Ogre::ColourValue::White);
+    down_2.colours[0] = Ogre::ColourValue::Blue;
+    down_2.colours[1] = Ogre::ColourValue::Red;
+    down_2.colours[2] = Ogre::ColourValue::White;
 
-    down_2.normals.push_back(down_normal);
-    down_2.normals.push_back(down_normal);
-    down_2.normals.push_back(down_normal);
+    down_2.normals[0] = down_normal;
+    down_2.normals[1] = down_normal;
+    down_2.normals[2] = down_normal;
 
     triangles.push_back(down_1);
     triangles.push_back(down_2);
@@ -345,30 +345,30 @@ std::vector<BlockTriangle> ComplexTower::getBlockTriangles(unsigned level, unsig
     Ogre::Vector3 outer_anti_normal = -inner_anti_normal;
 
     BlockTriangle forward_1;
-    forward_1.points.push_back(points.a2);
-    forward_1.points.push_back(points.a1);
-    forward_1.points.push_back(points.c1);
+    forward_1.points[0] = points.a2;
+    forward_1.points[1] = points.a1;
+    forward_1.points[2] = points.c1;
 
-    forward_1.colours.push_back(Ogre::ColourValue::White);
-    forward_1.colours.push_back(Ogre::ColourValue::Red);
-    forward_1.colours.push_back(Ogre::ColourValue::Blue);
+    forward_1.colours[0] = Ogre::ColourValue::White;
+    forward_1.colours[1] = Ogre::ColourValue::Red;
+    forward_1.colours[2] = Ogre::ColourValue::Blue;
 
-    forward_1.normals.push_back(outer_anti_normal);
-    forward_1.normals.push_back(outer_anti_normal);
-    forward_1.normals.push_back(outer_clock_normal);
+    forward_1.normals[0] = outer_anti_normal;
+    forward_1.normals[1] = outer_anti_normal;
+    forward_1.normals[2] = outer_clock_normal;
 
     BlockTriangle forward_2;
-    forward_2.points.push_back(points.c1);
-    forward_2.points.push_back(points.c2);
-    forward_2.points.push_back(points.a2);
+    forward_2.points[0] = points.c1;
+    forward_2.points[1] = points.c2;
+    forward_2.points[2] = points.a2;
 
-    forward_2.colours.push_back(Ogre::ColourValue::Blue);
-    forward_2.colours.push_back(Ogre::ColourValue::Green);
-    forward_2.colours.push_back(Ogre::ColourValue::White);
+    forward_2.colours[0] = Ogre::ColourValue::Blue;
+    forward_2.colours[1] = Ogre::ColourValue::Green;
+    forward_2.colours[2] = Ogre::ColourValue::White;
 
-    forward_2.normals.push_back(outer_clock_normal);
-    forward_2.normals.push_back(outer_clock_normal);
-    forward_2.normals.push_back(outer_anti_normal);
+    forward_2.normals[0] = outer_clock_normal;
+    forward_2.normals[1] = outer_clock_normal;
+    forward_2.normals[2] = outer_anti_normal;
 
     triangles.push_back(forward_1);
     triangles.push_back(forward_2);
