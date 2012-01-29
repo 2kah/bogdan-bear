49ccf33c3fa4b21ad30285a1432eeaccd595496c
Ragzouken <Ragzouken@riddler.castle.grayskull.co.uk>
2012-01-29 16:32:17 +0000

    Missing bounds on physics blocks caused physics updating problem. (reported by Joe)

--- Prototype/TowerPhysics.cpp
+++ Prototype/TowerPhysics.cpp
@@ -73,7 +73,7 @@ TowerPhysics::TowerPhysics(Tower *tower, btDiscreteDynamicsWorld *dynamicsWorld)
     
     for (unsigned level = 0; level < this->tower->levels / 4; ++level)
     {
-        this->chunks.push_back(new PhysicsChunk(tower, BoundingVolume(level * 4, (level + 1) * 4, 0, this->tower->layers, 0, 0), dynamicsWorld));
+        this->chunks.push_back(new PhysicsChunk(tower, BoundingVolume(level * 4, (level + 1) * 4, 0, this->tower->layers, 0, this->tower->sectors), dynamicsWorld));
     }
 
     //*/
