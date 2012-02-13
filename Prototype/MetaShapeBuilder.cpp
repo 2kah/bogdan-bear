cf6b05892801e97e87fdf7417c6f23122655d8fc
Adam Harwood <adam.2kah@gmail.com>
2012-02-13 01:55:24 +0000

    Platform meta shapes are now placed randomly within their chunk.

--- Prototype/MetaShapeBuilder.cpp
+++ Prototype/MetaShapeBuilder.cpp
@@ -21,14 +21,25 @@ void MetaShapeBuilder::getMetaShape(int type, BuilderChunk *chunk, MetaShape *me
 
 void MetaShapeBuilder::makePlatform(BuilderChunk *chunk, MetaShape *metaShape)
 {
-	int height = (rand() % (chunk->bounds.level_top - chunk->bounds.level_bottom)) + 1;
-	int depth = (rand() % (chunk->bounds.layer_outer - chunk->bounds.layer_inner)) + 1;
-	int width = (rand() % (chunk->bounds.sector_right - chunk->bounds.sector_left)) + 1;
-	for(int levels = 0; levels < height; levels++)
+	//randomly generate the size of the platform
+	unsigned height = (rand() % (chunk->bounds.level_top - chunk->bounds.level_bottom)) + 1;
+	unsigned depth = (rand() % (chunk->bounds.layer_outer - chunk->bounds.layer_inner)) + 1;
+	unsigned width = (rand() % (chunk->bounds.sector_right - chunk->bounds.sector_left)) + 1;
+
+	//randomly generate the placement of the platform within the chunk (guard against 0 modulus)
+	unsigned levelPlacement = ((chunk->bounds.level_top - chunk->bounds.level_bottom) - height) == 0 ? 0 : rand() % ((chunk->bounds.level_top - chunk->bounds.level_bottom) - height);
+	unsigned layerPlacement = ((chunk->bounds.layer_outer - chunk->bounds.layer_inner) - depth) == 0 ? 0 : rand() % ((chunk->bounds.layer_outer - chunk->bounds.layer_inner) - depth);
+	unsigned sectorPlacement = ((chunk->bounds.sector_right - chunk->bounds.sector_left) - width) == 0 ? 0 : rand() % ((chunk->bounds.sector_right - chunk->bounds.sector_left) - width);
+
+	height += levelPlacement;
+	depth += layerPlacement;
+	width += sectorPlacement;
+
+	for(int levels = levelPlacement; levels < height; levels++)
 	{
-		for(int layers = 0; layers < depth; layers++)
+		for(int layers = layerPlacement; layers < depth; layers++)
 		{
-			for(int sectors = 0; sectors < width; sectors++)
+			for(int sectors = sectorPlacement; sectors < width; sectors++)
 			{
 				metaShape->coords.push_back(Triple(levels + chunk->bounds.level_bottom, layers + chunk->bounds.layer_inner, sectors + chunk->bounds.sector_left));
 			}
