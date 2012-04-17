ec8d8d1dc1f60e6fab73abb111f7571127d24b97
Adam Harwood <adam.2kah@gmail.com>
2012-04-17 15:36:19 +0100

    Switched to using a boost pseudo random number generator for all tower regen randomness

--- Prototype/MetaShapeBuilder.h
+++ Prototype/MetaShapeBuilder.h
@@ -7,12 +7,14 @@ class MetaShapeBuilder
 {
 public:
 
-	MetaShapeBuilder();
+	MetaShapeBuilder(boost::random::mt19937 &gen);
 	virtual ~MetaShapeBuilder();
 
 	virtual void getMetaShape(int type, BuilderChunk *chunk, MetaShape *metaShape);
 
 	virtual void makePlatform(BuilderChunk *chunk, MetaShape *metaShape);
+
+	boost::random::mt19937 gen;
 };
 
 #endif // #ifndef __MetaShapeBuilder_h_
