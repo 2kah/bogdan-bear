a1a6130bc8a40016307c50959ea523df6b8f408f
Adam Harwood <adam.2kah@gmail.com>
2012-04-21 16:46:56 +0100

    Stopped the camera from being able to look back through you. Currently can look slightly behind you when looking down to make rocket jumping easier

--- Prototype/Player.cpp
+++ Prototype/Player.cpp
@@ -52,6 +52,12 @@ void Player::update()
 	this->orientation = this->orientation * Ogre::Quaternion(Ogre::Radian(rotX), Ogre::Vector3::UNIT_Y);
     this->relativeAim = this->relativeAim * Ogre::Quaternion(Ogre::Radian(rotY), Ogre::Vector3::UNIT_X);
 
+	if(this->relativeAim.getPitch() > Ogre::Radian(Ogre::Math::PI / 2))
+		this->relativeAim = Ogre::Quaternion(Ogre::Radian(Ogre::Math::PI / 2), Ogre::Vector3::UNIT_X);
+	
+	if(this->relativeAim.getPitch() < Ogre::Radian(-Ogre::Math::PI / 1.8))
+		this->relativeAim = Ogre::Quaternion(Ogre::Radian(-Ogre::Math::PI / 1.8), Ogre::Vector3::UNIT_X);
+
     rotX = 0;
     rotY = 0;
     
